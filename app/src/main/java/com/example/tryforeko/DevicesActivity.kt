ad6c9d4b09ac5ce6907473f1a1d5aa2c0a235b02
MatthewFairchild24 <matfray804@gmail.com>
2024-11-15 03:08:18 +0300

    first_design_try

---
+++ app/src/main/java/com/example/tryforeko/DevicesActivity.kt
@@ -0,0 +1,31 @@
+package com.example.tryforeko
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class DevicesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_devices)
+
+        val buttonMe = findViewById<Button>(R.id.meButton)
+        buttonMe.setOnClickListener {
+            val intent = Intent(this, MeActivity::class.java)
+            startActivity(intent)
+        }
+
+        val buttonDevice = findViewById<Button>(R.id.deviceButton)
+        buttonDevice.setOnClickListener {
+            val intent = Intent(this, DevicesActivity::class.java)
+            startActivity(intent)
+        }
+
+        val buttonTracker = findViewById<Button>(R.id.trackerButton)
+        buttonTracker.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/tryforeko/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.tryforeko
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val buttonMe = findViewById<Button>(R.id.meButton)
+        buttonMe.setOnClickListener {
+            val intent = Intent(this, MeActivity::class.java)
+            startActivity(intent)
+        }
+
+        val buttonDevice = findViewById<Button>(R.id.deviceButton)
+        buttonDevice.setOnClickListener {
+            val intent = Intent(this, DevicesActivity::class.java)
+            startActivity(intent)
+        }
+
+        val buttonTracker = findViewById<Button>(R.id.trackerButton)
+        buttonTracker.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
